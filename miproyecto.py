38e0b31071541f0630a6ff4802081ffa0c3e3cbe
Yuba <ulateyuba@gmail.com>
2023-06-12 23:56:13 -0600

    actuaizacion de internas y funciones

--- miproyecto.py
+++ miproyecto.py
@@ -1,6 +1,6 @@
 from agenda import agenda as Agenda, lista as Lista
 from datetime import datetime
-from typing import Self, Optional
+from typing import Any, Self, Optional
 import tkinter
 import customtkinter
 
@@ -37,15 +37,18 @@ class MiLista(Lista):
             raiz.sig = nodo
         else:
             self._agregar(raiz.sig, nodo)
-    def imprimir_lista(self,nodo):
-        self._imprimir_lista(self)
-    def _imprimir_lista(self,lista: Lista) -> None:
-        list=[]
+
+    def imprimir_lista(self, nodo) -> str:
+        return (self._imprimir_lista(nodo))
+
+    def _imprimir_lista(self, lista) -> None:
+        result = []
         nodo = lista
         while nodo:
-            list.append(nodo)
+            result.append(str(nodo))  # Agregar representación legible del objeto
+            print(str(nodo))
             nodo = nodo.sig
-        return(list)
+        return (result[1:])
 class MiAgenda(MiLista):
     def __init__(self,titulo:str,fecha:datetime) -> None:
         super().__init__()
@@ -64,8 +67,11 @@ class MiPersona(MiLista):
         self.apellido1 = apellido1
         self.apellido2 = apellido2
 
+
+
     def __str__(self) -> str:
         return ("{0} {1} {2}".format(self.nombre, self.apellido1, self.apellido2))
+    
 class MiApartado(MiLista):
     def __init__(self,nombre:str,descripcion:str) -> None:
         super().__init__()
@@ -115,72 +121,88 @@ def inter(): #se usa el ejemplo de CTk como base para la interfaz
     customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
     app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-    app.geometry("850x700")
+    app.geometry("500x500")
     app.title("Tercer proyecto programado")
     miAgenda= MiAgenda
     miPersona=MiPersona(None,None,None)
-    miApartado=MiApartado
-    miPunto=MiPunto
-    miDiscusion=MiDiscusion
-
-    #Tabs
-    tabview = customtkinter.CTkTabview(master=app,height=500,width=500)
-    tabview.pack()
-    tabview.add("Agenda") 
-    tabview.add("Participantes") 
-    tabview.add("Apartados")  
-    tabview.add("Puntos")  
-    tabview.add("Discusiones") 
-    tabview.place_configure(relx=0.2,rely=0.005,height=700, width=750)
+    miApartado=MiApartado(None,None)
+    miPunto=MiPunto(None,None)
+    global miDiscusion
+    miDiscusion=MiDiscusion(None,None)
 
     #funciones de la interfaz
     def button_function1():
-        global miAgenda
+        """
+        Botón para crear la Agenda
+        """
         dialog = customtkinter.CTkInputDialog(text="Titulo de la agenda:", title="Agenda")
         text = dialog.get_input()  # waits for input
         miAgenda=MiAgenda(fecha=datetime.today(),titulo=text)
-        textbox_agenda = customtkinter.CTkTextbox(master=tabview.tab("Agenda"), width=400, corner_radius=0)
-        textbox_agenda.insert("0.0",miAgenda.titulo )
-        textbox_agenda.configure(state="disabled")  
-        textbox_agenda.pack(padx=50, pady=100)
+        miAgenda.agregar(miAgenda)
+        label = customtkinter.CTkLabel(app, text="Titulo:{0}\n Hora de creacion:{1}".format(miAgenda.titulo,miAgenda.fecha), fg_color="transparent")
+        label.pack(padx=0.4)
     def button_function2():
-        pass
-    def button_function3():#BOTON para añadir un participante
+        """
+        Botón para añadir una discusion
+        """
+        global miDiscusion
+        dialog = customtkinter.CTkInputDialog(text="Transcripcion:", title="Transcripcion")
+        text1 = dialog.get_input()
+        dialog = customtkinter.CTkInputDialog(text="Nombre del participante:", title="Participante")
+        text2 = dialog.get_input()
+        if miPersona.buscar(lambda miPersona: miPersona.nombre == text2):
+            miDiscusion.agregar(MiDiscusion(text1,text2))
+            miDiscusion.imprimir_lista(miDiscusion)
+        else:
+            dialog = customtkinter.CTkInputDialog(text="El participante no existe", title="ERROR")
+   
+
+    def button_function3():
+        """
+        Botón para añadir un participante
+        """
         dialog = customtkinter.CTkInputDialog(text="Nombre:", title="Participante")
         text1 = dialog.get_input()
         dialog = customtkinter.CTkInputDialog(text="Apellido 1:", title="Participante")
         text2 = dialog.get_input()
         dialog = customtkinter.CTkInputDialog(text="Apellido 2:", title="Participante")
         text3 = dialog.get_input()
-        miPersona.agregar(MiPersona(text1.capitalize(), text2.capitalize(), text3.capitalize()))
-        textbox_participantes = customtkinter.CTkTextbox(master=tabview.tab("Participantes"), width=400, corner_radius=0)
-        textbox_participantes.insert("0.0", "{0}".format(miPersona.imprimir_lista(miPersona)))
-        textbox_participantes.configure(state="disabled")
-        textbox_participantes.pack(padx=50, pady=100)
+        miPersona.agregar(MiPersona(text1.capitalize(),text2.capitalize(),text3.capitalize()))
+        miPersona.imprimir_lista(miPersona) 
+
     def button_function4():
+        """
+        Botón para añadir un apartado
+        """
         dialog = customtkinter.CTkInputDialog(text="Nombre:", title="Apartado")
         text1 = dialog.get_input()
         dialog = customtkinter.CTkInputDialog(text="Descripcion:", title="Apartado")
         text2 = dialog.get_input()
-        miApartado.agregar(text1.capitalize,text2.capitalize)
-        textbox = customtkinter.CTkTextbox(master=tabview.tab("Apartados"), width=400, corner_radius=0)
-        textbox.insert("0.0","{0}".format(miAgenda.participantes._asList))
-        textbox.configure(state="disabled")  
-        textbox.pack(padx=50, pady=100)
+        miApartado.agregar(MiApartado(text1.capitalize,text2.capitalize))
 
+    def button_function5():
+        """
+        Botón para añadir un punto
+        """
+        dialog = customtkinter.CTkInputDialog(text="Nombre:", title="Apartado")
+        text1 = dialog.get_input()
+        dialog = customtkinter.CTkInputDialog(text="Descripcion:", title="Apartado")
+        text2 = dialog.get_input()
+    def button_function6():
+        pass
     #Botones
     button_Agenda = customtkinter.CTkButton(master=app, text="Crear la agenda", command=button_function1)
-    button_Agenda.place(relx=0.1, rely=0.1, anchor=tkinter.CENTER)
-    button2 = customtkinter.CTkButton(master=app, text="holder", command=button_function2)
-    button2.place(relx=0.1, rely=0.9, anchor=tkinter.CENTER)
-    button_Participantes = customtkinter.CTkButton(master=tabview.tab("Participantes"), text="Agregar participante", command=button_function3)
-    button_Participantes.configure(width=80, height=30)
-    button_Participantes.place(relx=0.85, rely=0.1, anchor=tkinter.CENTER)
-    button_apartados = customtkinter.CTkButton(master=tabview.tab("Apartados"), text="Agregar Apartado", command=button_function4)
-    button_apartados.configure(width=80, height=30)
-    button_apartados.place(relx=0.85, rely=0.1, anchor=tkinter.CENTER)
-    button_tab = customtkinter.CTkButton(master=tabview.tab("Puntos"), text="button tab 2")
-    button_tab.place(relx=0.7)
+    button_Agenda.place(relx=0.5, rely=0.1, anchor=tkinter.CENTER)
+    button_discusion = customtkinter.CTkButton(master=app, text="Agregar discusión", command=button_function2)
+    button_discusion.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+    button_Participantes = customtkinter.CTkButton(master=app, text="Agregar participante", command=button_function3)
+    button_Participantes.place(relx=0.5, rely=0.2, anchor=tkinter.CENTER)
+    button_apartados = customtkinter.CTkButton(master=app, text="Agregar Apartado", command=button_function4)
+    button_apartados.place(relx=0.5, rely=0.3, anchor=tkinter.CENTER)
+    button_punto = customtkinter.CTkButton(master=app, text="Agregar Punto",command=button_function5)
+    button_punto.place(relx=0.5,rely=0.4,anchor=tkinter.CENTER)
+    button_HTML = customtkinter.CTkButton(master=app, text="Generar HTML",command=button_function6)
+    button_HTML.place(relx=0.5,rely=0.6,anchor=tkinter.CENTER)
 
     app.mainloop()
 inter()
